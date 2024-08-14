43e09340b1b391002badc556a3598ba9408fb412
Guillermo Álvarez <guillermo@cientifico.net>
2024-08-15 00:17:25 +0200

    Updates

--- gen.sh
+++ gen.sh
@@ -1,66 +1,49 @@
-#!/bin/bash 
+#!/bin/bash
 
+REPOS="autocerts flattenfs lazycontext lazyservice layerfs lazyapp lazyassets lazycontroller lazydispatch lazyhttp lazyml lazysupport lazyview memfs multihttp protocolmux router"
 
-
-REPOS="autocerts flattenfs golazy layerfs lazyapp lazyassets lazycontroller lazydispatch lazyhttp lazyml lazysupport lazyview memfs multihttp protocolmux router"
-
-
-for repo in $REPOS; do
-	FILE=$repo.html
-
-	cat > $FILE <<-EOF
+read -d '' HTMLHEAD <<'EOF'
 	<!DOCTYPE html>
 	<html lang=en>
 	  <head>
-	    <title>GoLazy</title>
-	       <meta name=go-import content="golazy.dev/$repo git https://github.com/golazy/$repo.git">
-	       <meta name=go-source content="golazy.dev/$repo https://github.com/golazy/$repo https://github.com/golazy/$repo/tree/master{/dir} https://github.com/golazy/$repo/blob/master{/dir}/{file}#L{line}">
-	    <style>
-	      html {
-		max-width: 70ch;
-		padding: 3em 1em;
-		margin: auto;
-		line-height: 1.75;
-		font-size: 1.25em;
-	      }
-	    </style>
+	    <link rel="stylesheet" href="bulma.css" >
+EOF
+
+for repo in $REPOS; do
+  FILE=$repo.html
+
+  cat >$FILE <<-EOF
+$HTMLHEAD
+       <title>GoLazy</title>
+       <meta name=go-import content="golazy.dev/$repo git https://github.com/golazy/$repo.git">
+       <meta name=go-source content="golazy.dev/$repo https://github.com/golazy/$repo https://github.com/golazy/$repo/tree/master{/dir} https://github.com/golazy/$repo/blob/master{/dir}/{file}#L{line}">
 	  <body>
 	    <h1>golazy.dev/$repo</h1>
-	    <a href="https://github.com/golazy/$repo">Golazy $repo</a>
-	    <a href="https://pkg.go.dev/golazy.dev/$repo">godocs</a>
+      <a href="https://github.com/golazy/$repo"><img src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white" alt="Github"></a>
+      <a href="https://pkg.go.dev/golazy.dev/$repo"><img src="https://pkg.go.dev/badge/golazy.dev/$repo.svg" alt="Go Reference"></a>
 	    
 
 	EOF
 done
 
-
-
-
 FILE=index.html
-cat > $FILE <<-EOF
-<!DOCTYPE html>
-<html lang=en>
-  <head>
+cat >$FILE <<-EOF
+$HTMLHEAD
     <title>GoLazy</title>
-    <style>
-      html {
-	max-width: 70ch;
-	padding: 3em 1em;
-	margin: auto;
-	line-height: 1.75;
-	font-size: 1.25em;
-      }
-    </style>
   <body>
     <h1>golazy.dev</h1>
     <ul>
 EOF
 
 for repo in $REPOS; do
-	cat >> $FILE <<-EOF
-              <li><a href="$repo.html">$repo</a> <a href="https://github.com/golazy/$repo">github</a> <a href="https://pkg.go.dev/golazy.dev/$repo">godoc</a></li>
+  cat >>$FILE <<-EOF
+              <li>
+              <a href="$repo.html">$repo</a> 
+              <a href="https://github.com/golazy/$repo"><img src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white" alt="Github"></a>
+              <a href="https://pkg.go.dev/golazy.dev/$repo"><img src="https://pkg.go.dev/badge/golazy.dev/$repo.svg" alt="Go Reference"></a>
+
+
+              </li>
+
 EOF
 done
-
-
-
